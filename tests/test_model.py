15e9f5358b5530040e91b7fd02308c97a6b0a15d
Ramya <ramyarao@outlook.in>
2025-08-06 17:12:45 +0530

    changes in test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.pipeline import read_and_split
 from src.modelling import model_and_evaluate
 import pandas as pd
+import numpy as np
 
 def test_model():
     X_train, X_test, y_train, y_test = read_and_split()
@@ -15,7 +16,7 @@ def test_model():
 
     # Step 3: Simple functional checks
     assert len(predictions)>0, "Predictions should not be empty"
-    assert isinstance(predictions, list), "Predictions should be a list"
-    assert len(predictions) == len(y_test), "Predictions and test data should have same length"
+    assert isinstance(predictions, np.ndarray), "Predictions should be a NumPy array."
+    assert predictions.shape[0] == y_test.shape[0], "Predictions and y_test should have same number of samples"
     assert isinstance(accuracy, float), "Accuracy sould be a float"
     print("✅ All basic model checks passed!")
